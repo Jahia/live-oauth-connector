734ecb18969800a11b977413ee1fdee8bdb483bd
Florent BOURASSE <fbourasse@jahia.com>
2018-03-13 09:09:18 +0100

    Remove of the license check

--- src/main/java/org/jahia/modules/liveoauthconnector/impl/LiveConnectorImpl.java
+++ src/main/java/org/jahia/modules/liveoauthconnector/impl/LiveConnectorImpl.java
@@ -3,19 +3,12 @@ package org.jahia.modules.liveoauthconnector.impl;
 import java.util.List;
 import java.util.Map;
 import org.jahia.modules.jahiaoauth.service.ConnectorService;
-import org.jahia.security.license.LicenseCheckException;
 
 public class LiveConnectorImpl implements ConnectorService {
 
     private String protectedResourceUrl;
     private String serviceName;
     private List<Map<String, Object>> availableProperties;
-
-    public void onStart() throws LicenseCheckException {
-//        if (!LicenseCheckerService.Stub.isAllowed("org.jahia.oauthLive")) {
-//            throw new LicenseCheckException("Your DX license does not allow you to run Live OAuth Connector. please contact Jahia Solutions.");
-//        }
-    }
 
     @Override
     public String getProtectedResourceUrl() {
